2516d74f019038dfb33626cf2509208ed1a70f62
arya-sa <rizkyaryas11@gmail.com>
2023-07-18 20:39:19 +0700

    add location options queries

--- graphql/queries/fetch-queries.ts
+++ graphql/queries/fetch-queries.ts
@@ -1,31 +1,51 @@
 import { gql } from "@apollo/client";
 
 export const GET_CHARACTERS_SPECIES = gql`
-    query GetCharactersSpecies {
-        characters {
-            results {
-                species
-            }
-        }
+  query GetCharactersSpecies {
+    characters {
+      results {
+        species
+      }
     }
-`
+  }
+`;
 
 export const GET_CHARACTERS_GENDER = gql`
-    query GetCharactersGender {
-        characters {
-            results {
-                gender
-            }
-        }
+  query GetCharactersGender {
+    characters {
+      results {
+        gender
+      }
     }
-`
+  }
+`;
 
 export const GET_CHARACTERS_STATUS = gql`
-    query GetCharactersStatus {
-        characters {
-            results {
-                status
-            }
-        }
+  query GetCharactersStatus {
+    characters {
+      results {
+        status
+      }
     }
-`
+  }
+`;
+
+export const GET_LOCATION_TYPE = gql`
+  query GetLocationType {
+    locations {
+      results {
+        type
+      }
+    }
+  }
+`;
+
+export const GET_LOCATION_DIMENSION = gql`
+  query GetLocationDimension {
+    locations {
+      results {
+        dimension
+      }
+    }
+  }
+`;
